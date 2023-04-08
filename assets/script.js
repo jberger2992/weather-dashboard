a3f01f1273ae70df9aa278376f7ecb1a44beda56
jberger2992 <jberger2992@gmail.com>
2023-04-08 12:45:05 -0400

    basic api and dom work

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,27 @@
-var asd = "https://api.openweathermap.org/data/2.5/forecast?q=indianapolis&appid=4c34d8d8ec08ac9c00d227658c97fec3";
+var searchedCityH2 = document.getElementById("searchedcity");
+var searchedTempP = document.getElementById("searchedtemp");
+var searchedWindP = document.getElementById("searchedwind");
+var searchedHumidityP = document.getElementById("searchedhumidity");
+var searchBtn = document.getElementById("citysearchbtn");
+var searchInput = document.getElementById("citynameinput");
+
+var APIKey = "4c34d8d8ec08ac9c00d227658c97fec3";
+var city = "";
+var queryURL = "";
+var asd = "";
+function search(){
+fetch(queryURL)
+.then(function (response) {
+    return response.json();
+  })
+    .then(function (data){
+        console.log(data);
+        searchedTempP.textContent ="Temperature: " + data.main.temp;
+        searchedWindP.textContent ="Wind: " + data.wind.speed;
+        searchedHumidityP.textContent ="Humidity: " + data.main.humidity;
+    })
+
+
 
 fetch(asd)
 .then(function (response) {
@@ -7,3 +30,11 @@ fetch(asd)
     .then(function (data){
         console.log(data);
     })
+  }
+  searchBtn.addEventListener("click", function(){
+    city = searchInput.value;
+    queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    asd = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
+    searchedCityH2.textContent = "Current weather in " + city;
+    search();
+  })
