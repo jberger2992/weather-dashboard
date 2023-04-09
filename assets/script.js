36726925c1113dc449c817abf7236e5162b03618
jberger2992 <jberger2992@gmail.com>
2023-04-08 21:32:56 -0400

    html and API work for extra days

--- assets/script.js
+++ assets/script.js
@@ -4,37 +4,83 @@ var searchedWindP = document.getElementById("searchedwind");
 var searchedHumidityP = document.getElementById("searchedhumidity");
 var searchBtn = document.getElementById("citysearchbtn");
 var searchInput = document.getElementById("citynameinput");
+var oneH3 = document.getElementById("onedate");
+var oneTempP = document.getElementById("onetemp");
+var oneWindP = document.getElementById("onewind");
+var oneHumidityP = document.getElementById("onehumidity");
+var twoH3 = document.getElementById("twodate");
+var twoTempP = document.getElementById("twotemp");
+var twoWindP = document.getElementById("twowind");
+var twoHumidityP = document.getElementById("twohumidity");
+var threeH3 = document.getElementById("threedate");
+var threeTempP = document.getElementById("threetemp");
+var threeWindP = document.getElementById("threewind");
+var threeHumidityP = document.getElementById("threehumidity");
+var fourH3 = document.getElementById("fourdate");
+var fourTempP = document.getElementById("fourtemp");
+var fourWindP = document.getElementById("fourwind");
+var fourHumidityP = document.getElementById("fourhumidity");
+var fiveH3 = document.getElementById("fivedate");
+var fiveTempP = document.getElementById("fivetemp");
+var fiveWindP = document.getElementById("fivewind");
+var fiveHumidityP = document.getElementById("fivehumidity");
 
 var APIKey = "4c34d8d8ec08ac9c00d227658c97fec3";
 var city = "";
 var queryURL = "";
-var asd = "";
 function search(){
-fetch(queryURL)
-.then(function (response) {
-    return response.json();
-  })
-    .then(function (data){
-        console.log(data);
-        searchedTempP.textContent ="Temperature: " + data.main.temp;
-        searchedWindP.textContent ="Wind: " + data.wind.speed;
-        searchedHumidityP.textContent ="Humidity: " + data.main.humidity;
+  var lat = "";
+  var lon = "";
+  fetch(queryURL)
+  .then(function (response) {
+      return response.json();
     })
+      .then(function (data){
+          console.log(data);
+          lat = data.coord.lat;
+          lon = data.coord.lon;
+          searchedTempP.textContent = "Temperature:  " + Math.round(((data.main.temp)-273.15)*9/5+32) + "° F";
+          searchedWindP.textContent = "Wind:  " + data.wind.speed + " mph";
+          searchedHumidityP.textContent = "Humidity:  " + data.main.humidity + "%";
+          searchTwo()
+      })
 
-
-
-fetch(asd)
-.then(function (response) {
-    return response.json();
-  })
-    .then(function (data){
-        console.log(data);
+      
+  function searchTwo(){
+    var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+  fetch(fiveDay)
+  .then(function (response) {
+      return response.json();
     })
+      .then(function (data){
+          console.log(data);
+          oneH3.textContent = data.list[6].dt_txt.slice(5,10);
+          oneTempP.textContent = "Temperature:  " + Math.round(((data.list[6].main.temp)-273.15)*9/5+32) + "° F";
+          oneWindP.textContent =  "Wind:  " + data.list[6].wind.speed + " mph";
+          oneHumidityP.textContent = "Humidity:  " + data.list[6].main.humidity + "%";
+          twoH3.textContent = data.list[14].dt_txt.slice(5,10);
+          twoTempP.textContent = "Temperature:  " + Math.round(((data.list[14].main.temp)-273.15)*9/5+32) + "° F";
+          twoWindP.textContent =  "Wind:  " + data.list[14].wind.speed + " mph";
+          twoHumidityP.textContent = "Humidity:  " + data.list[14].main.humidity + "%";
+          threeH3.textContent = data.list[22].dt_txt.slice(5,10);
+          threeTempP.textContent = "Temperature:  " + Math.round(((data.list[22].main.temp)-273.15)*9/5+32) + "° F";
+          threeWindP.textContent =  "Wind:  " + data.list[22].wind.speed + " mph";
+          threeHumidityP.textContent = "Humidity:  " + data.list[22].main.humidity + "%";
+          fourH3.textContent = data.list[30].dt_txt.slice(5,10);
+          fourTempP.textContent = "Temperature:  " + Math.round(((data.list[30].main.temp)-273.15)*9/5+32) + "° F";
+          fourWindP.textContent =  "Wind:  " + data.list[30].wind.speed + " mph";
+          fourHumidityP.textContent = "Humidity:  " + data.list[30].main.humidity + "%";
+          fiveH3.textContent = data.list[38].dt_txt.slice(5,10);
+          fiveTempP.textContent = "Temperature:  " + Math.round(((data.list[38].main.temp)-273.15)*9/5+32) + "° F";
+          fiveWindP.textContent =  "Wind:  " + data.list[38].wind.speed + " mph";
+          fiveHumidityP.textContent = "Humidity:  " + data.list[38].main.humidity + "%";
+          
+      })
   }
+}
   searchBtn.addEventListener("click", function(){
     city = searchInput.value;
     queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    asd = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
     searchedCityH2.textContent = "Current weather in " + city;
     search();
   })
